05f608a2fd5c8ba7497af3ce2b047e7562572955
Amit Gupta <amitguptacse2028@gmail.com>
2026-09-02 19:37:06 +0530

    Time: 3 ms (86.02%) | Memory: 19.8 MB (33.40%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int target,int d1,int tsum,int idx){
+    int helper(vector<int>& nums, int target,int d1,int tsum,int idx,vector<vector<int>>&dp){
         if(idx==nums.size()){
             if(2*d1-tsum==target) return 1;
             else return 0;
 
 
         }
+        if(dp[idx][d1]!= -1)  return dp[idx][d1];
+        int pickup = helper(nums,target,d1+nums[idx],tsum,idx+1,dp);
+        int notpickup=helper(nums,target,d1,tsum,idx+1,dp);
 
-        int pickup = helper(nums,target,d1+nums[idx],tsum,idx+1);
-        int notpickup=helper(nums,target,d1,tsum,idx+1);
-
-        return pickup +notpickup;
+        return dp[idx][d1]= pickup +notpickup;
 
 
     }
@@ -19,6 +19,7 @@ public:
         int n = nums.size();
         int sum=0;
         for(auto el : nums) sum+=el;
-        return helper( nums,target,0,sum,0);
+        vector<vector<int>> dp(n+1,vector<int> (sum+1,-1));
+        return helper( nums,target,0,sum,0,dp);
     }
 };
