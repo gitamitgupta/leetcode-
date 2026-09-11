5856b48c8cb3b217715e9ee0c730e4506c56341a
Amit Gupta <amitguptacse2028@gmail.com>
2026-09-11 18:01:40 +0530

    Time: 34 ms (30.20%) | Memory: 27 MB (58.81%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int helper(string s, string t,int i ,int j, vector<vector<int>> &dp){
+    int helper(string &s, string &t,int i ,int j, vector<vector<int>> &dp){
         if(j<0) return 1;
         if(i<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
